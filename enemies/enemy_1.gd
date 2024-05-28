ce6eea8646e4f858985a14ec94adcdcd8323fe33
RyanDeBarros <ryan2004db@gmail.com>
2024-05-27 22:12:06 -0400

    enemy face position responds to player position

--- enemies/enemy_1.gd
+++ enemies/enemy_1.gd
@@ -4,10 +4,20 @@ extends RigidBody2D
 
 @export var hit_strength := 3000.0
 @export var reposte_susceptibility := 10.0
+@export var face_max_x := 20.0
 
 @onready var level := get_tree().get_first_node_in_group(&"level") as Level
+@onready var face: Node2D = $Face
 @onready var hit_area_l: Area2D = $HandL/HitAreaL
 @onready var hit_area_r: Area2D = $HandR/HitAreaR
+@onready var player := get_tree().get_first_node_in_group(&"player") as Player
+
+
+func _process(delta: float) -> void:
+	var player_delta := clampf(player.global_position.x - global_position.x,\
+			-face_max_x, face_max_x)
+	face.position.x = player_delta
+	face.scale.x = signf(player_delta)
 
 
 func _on_hit_area_l_body_entered(body: Node2D) -> void:
