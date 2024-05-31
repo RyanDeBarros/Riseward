aa141aceec72dd3644a7692b1148258fc0812f8c
RyanDeBarros <ryan2004db@gmail.com>
2024-05-31 18:18:12 -0400

    finished tutorial

--- ui/tutorial_screen.gd
+++ ui/tutorial_screen.gd
@@ -4,6 +4,7 @@ extends Control
 
 @export var pages: Array[Control]
 
+@onready var next_btn: Button = $NextBTN
 @onready var current_page := 0
 
 
@@ -16,5 +17,7 @@ func _on_next_btn_pressed() -> void:
 	if current_page < len(pages):
 		pages[current_page - 1].visible = false
 		pages[current_page].visible = true
+		if current_page == len(pages) - 1:
+			next_btn.visible = false
 	else:
 		get_tree().change_scene_to_packed(Scenes.TITLE_SCREEN)
